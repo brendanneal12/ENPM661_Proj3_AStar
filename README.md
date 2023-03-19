6786ac4cce80f8b407cb0896dceb03698309d114
brendanneal12 <125323287+brendanneal12@users.noreply.github.com>
2023-03-19 11:17:44 -0400

    Readme + commenting

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # ENPM661_Proj3_AStar
 UMD ENPM661 Project 3 A-Star
+
+# Student Information
+Authors: Brendan Neal and Adam Lobo
+
+# Project Information
+Goal: Solve a maze using the A* Algorithm
+
+File Name: a_start_adam_brendan.py
+
+Recommended IDE: Visual Studio Code
+
+Python Version: 3
+
+# Github Repository Links
+
+Brendan: https://github.com/brendanneal12/ENPM661_Proj3_AStar
+
+Adam: https://github.com/AdazInventionz/ENPM661-Project-3
+
+# Libraries Used
+numpy, from matplotlib: pyplot, opencv, math, timeit, from queue: PriorityQueue
+
+# How to Run Code
+
+1. Hit Run
+2. Prompted by the terminal, enter the initial state X, Y, and Theta with spaces separating them. Theta should be a multiple of 30 (0,30,60,270,etc.)
+3. If your initial state is in an obstacle or outside of the workspace, you will be prompted to restart.
+4. Prompted by the terminal, enter the goal state X, Y, and Theta with spaces separating them. Theta should be a multiple of 30 (0,30,60,270,etc.)
+5. If your goal state is in an obstacle or outside of the workspace, you will be prompted to restart.
+6. Enter the Robot Radius
+7. Enter the Step Size.
+8. Observe the obstacle space setup. Obstacles are white, the desired clearance (5 pixels) is in light gray, and the additional robot radius clearance is in dark grey.
+9. Close the window to begin the A* search.
+10. While the search is running, the current node state (popped from the open list) is printed to the terminal.
+11. Once the goal is reached, the total cost and the time it took to search will be printed to the terminal. The final map will be displayed to your screen with the searched nodes in green, and the optimal path in magenta.
+12. Close the window to start the visualization.
+13. Once the visualization is complete, close the window to end the program.
+
+# IMPORTANT NOTES PLEASE READ
+1. For cases far away the start point, the search can take upwards of 10-15 minutes. We're not generating repeat nodes, but we have noticed that getting through the two rectangles is a bit of a challenge. Please be patient.
+2. The visualization is also quite slow. Please be patient.
+3. For smaller step sizes, the search appears to be  large blob, but if you zoom in you can see the individual moves quite well.
+4. Please click the following link in order to view an example output video that is produced by our code. Initial State is 5 5, Goal State is 300 225. I cannot commit the video directly to GitHub since the file size is way too large: https://drive.google.com/drive/u/0/folders/11FW4Fq-76BoGIyPZIVHYzmYJJwSpE-TE
+
+
--- a_star_adam_brendan.py
+++ a_star_adam_brendan.py
@@ -11,11 +11,10 @@ import queue
 from queue import PriorityQueue
 
 
-##------------Defining Node Class (From Previous Lab)-----------------##
+##------------Defining Node Class (From Previous Project)-----------------##
 
 class Node():
     #Initializing Function
-
     def __init__(self, state, parent, move, C2C, TotalCost):
         self.state = state
         self.parent = parent
@@ -35,13 +34,13 @@ class Node():
             return None
         return self.ReturnParent().ReturnState()
     
-    def ReturnMove(self):
+    def ReturnMove(self): #Returns Move
         return self.move
     
-    def ReturnC2C(self):
+    def ReturnC2C(self): # Returns C2C
         return self.C2C
     
-    def ReturnTotalCost(self): #Returns the Cost Leading up to the Node
+    def ReturnTotalCost(self): #Returns the Total Cost
         return self.TotalCost
 
     def __lt__(self, other): #OOP Definition for Less than. Required for Priority Queue.
@@ -66,7 +65,7 @@ class Node():
 def setup(robotradius):
 
     global arena
-    
+
     #Colors
     white = (255, 255, 255)
     gray = (177, 177, 177)
@@ -214,93 +213,94 @@ def checkValid(x, y, r):
 
 ##---------------------------------Defining my Action Set-------------------------------------##
 def MoveMaxTurnLeft(Current_State, Step_Size, RobotRadius):
-    RobotTheta = Current_State[2]
-    MoveTheta = RobotTheta + 60
 
-    if MoveTheta >=360:
+    RobotTheta = Current_State[2]
+    MoveTheta = RobotTheta + 60 #Adjust the Angle Offset
+
+    if MoveTheta >=360: #Wrapping to 0-360
         MoveTheta = MoveTheta -360
     if MoveTheta <0:
         MoveTheta = MoveTheta + 360
 
 
 
-    ChangeX = Step_Size * np.cos(np.radians(MoveTheta))
-    ChangeY = Step_Size * np.sin(np.radians(MoveTheta))
+    ChangeX = Step_Size * np.cos(np.radians(MoveTheta)) #Change in X
+    ChangeY = Step_Size * np.sin(np.radians(MoveTheta)) #Change in Y
 
-    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta]
-    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False:
+    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta] #Generate the New State
+    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False: #Check if move takes us into obstacle space, or outside the workspace.
         return None
 
     return NewNodeState
 
 def MoveTurnLeft(Current_State, Step_Size, RobotRadius):
     RobotTheta = Current_State[2]
-    MoveTheta = RobotTheta + 30
+    MoveTheta = RobotTheta + 30 #Angle Offset from current angle
 
-    if MoveTheta >=360:
+    if MoveTheta >=360: #Wrap to 0-360
         MoveTheta = MoveTheta -360
     if MoveTheta <0:
         MoveTheta = MoveTheta + 360
 
-    ChangeX = Step_Size * np.cos(np.radians(MoveTheta))
-    ChangeY = Step_Size * np.sin(np.radians(MoveTheta))
+    ChangeX = Step_Size * np.cos(np.radians(MoveTheta)) #Change in X
+    ChangeY = Step_Size * np.sin(np.radians(MoveTheta)) #Change in Y
 
-    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta]
-    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False:
+    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta] #Generate New State
+    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False: #Check if move takes us into obstacle space, or outside the workspace.
         return None
 
     return NewNodeState
 
 def MoveStraight(Current_State, Step_Size, RobotRadius):
     RobotTheta = Current_State[2]
-    MoveTheta = RobotTheta
+    MoveTheta = RobotTheta #Angle Offset
 
-    if MoveTheta >=360:
+    if MoveTheta >=360: #Wrap to 0-360
         MoveTheta = MoveTheta -360
     if MoveTheta <0:
         MoveTheta = MoveTheta + 360
 
-    ChangeX = Step_Size * np.cos(np.radians(MoveTheta))
-    ChangeY = Step_Size * np.sin(np.radians(MoveTheta))
+    ChangeX = Step_Size * np.cos(np.radians(MoveTheta)) #Change in X
+    ChangeY = Step_Size * np.sin(np.radians(MoveTheta)) #Change in Y
 
-    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta]
-    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False:
+    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta] #Generate New Node State
+    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False: #Check if move takes us into obstacle space, or outside the workspace.
         return None
 
     return NewNodeState
 
-def MoveMaxTurnRight(Current_State, Step_Size, RobotRadius):
+def MoveMaxTurnRight(Current_State, Step_Size, RobotRadius): 
     RobotTheta = Current_State[2]
-    MoveTheta = RobotTheta - 60
+    MoveTheta = RobotTheta - 60 #Angle Offset
 
-    if MoveTheta >=360:
+    if MoveTheta >=360: #Wrap to 360
         MoveTheta = MoveTheta -360
     if MoveTheta <0:
         MoveTheta = MoveTheta + 360
 
-    ChangeX = Step_Size * np.cos(np.radians(MoveTheta))
-    ChangeY = Step_Size * np.sin(np.radians(MoveTheta))
+    ChangeX = Step_Size * np.cos(np.radians(MoveTheta)) #Change in X
+    ChangeY = Step_Size * np.sin(np.radians(MoveTheta)) #Change in Y
 
-    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta]
-    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False:
+    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta] #Generate New Node State
+    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False: #Check if move takes us into obstacle space, or outside the workspace.
         return None
 
     return NewNodeState
 
 def MoveTurnRight(Current_State, Step_Size, RobotRadius):
     RobotTheta = Current_State[2]
-    MoveTheta = RobotTheta - 30
+    MoveTheta = RobotTheta - 30 #Angle Offset
 
-    if MoveTheta >=360:
+    if MoveTheta >=360: #Wrap to 360
         MoveTheta = MoveTheta -360
     if MoveTheta <0:
         MoveTheta = MoveTheta + 360
 
-    ChangeX = Step_Size * np.cos(np.radians(MoveTheta))
-    ChangeY = Step_Size * np.sin(np.radians(MoveTheta))
+    ChangeX = Step_Size * np.cos(np.radians(MoveTheta)) #Change in X
+    ChangeY = Step_Size * np.sin(np.radians(MoveTheta)) #Change in Y
 
-    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta]
-    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False:
+    NewNodeState = [Current_State[0] + ChangeX, Current_State[1] + ChangeY, MoveTheta] #Generate New Node State
+    if checkValid(NewNodeState[0], NewNodeState[1], RobotRadius) == False: #Check if move takes us into obstacle space, or outside the workspace.
         return None
 
     return NewNodeState
@@ -309,12 +309,14 @@ def MoveTurnRight(Current_State, Step_Size, RobotRadius):
 def GeneratePossibleMoves(Current_Node, StepSize, Robot_Radius):
     Current_Node_State = Current_Node.ReturnState()
     New_Node_Locations = []
+    #Append all new states to an array
     New_Node_Locations.append(MoveMaxTurnLeft(Current_Node_State, StepSize, Robot_Radius))
     New_Node_Locations.append(MoveTurnLeft(Current_Node_State, StepSize, Robot_Radius))
     New_Node_Locations.append(MoveStraight(Current_Node_State, StepSize, Robot_Radius))
     New_Node_Locations.append(MoveMaxTurnRight(Current_Node_State, StepSize, Robot_Radius))
     New_Node_Locations.append(MoveTurnLeft(Current_Node_State, StepSize, Robot_Radius))
-
+    
+    #If the action set is an invalid move, it returns none. This removes the "nones" from the possible new states
     Possible_New_States = [Location for Location in New_Node_Locations if Location is not None]
 
     return Possible_New_States
@@ -328,14 +330,14 @@ def Calculate_C2G(CurrentNodeState, GoalNodeState):
     X_Goal = GoalNodeState[0]
     Y_Goal = GoalNodeState[1]
     if CurrentNodeState is not None:
-        C2G = np.sqrt((X_Goal-X_Current)**2 + (Y_Goal- Y_Current)**2)
+        C2G = np.sqrt((X_Goal-X_Current)**2 + (Y_Goal- Y_Current)**2) #Euclidian Distance Heuristic function
     return C2G
 
 ##-----------------------Defining my Compare to Goal Function---------------------------##
 
 def CompareToGoal(Current_Node_Position, Goal_Node_Position, ErrorThreshold):
-    Dist2Goal = (Goal_Node_Position[0] - Current_Node_Position[0])**2 + (Goal_Node_Position[1] - Current_Node_Position[1])**2
-    if Dist2Goal < ErrorThreshold**2 and Current_Node_Position[2] == Goal_Node_Position[2]:
+    Dist2Goal = (Goal_Node_Position[0] - Current_Node_Position[0])**2 + (Goal_Node_Position[1] - Current_Node_Position[1])**2 #Euclidian Distance
+    if Dist2Goal < ErrorThreshold**2 and Current_Node_Position[2] == Goal_Node_Position[2]: #Error less than threshold PLUS the angle has to be equal
         return True
     else:
         return False
@@ -423,28 +425,28 @@ WSColoring(arena, GoalState, (0,255,0))
 plt.imshow(arena)
 plt.show()
 
-
+#Initialize Arena and Thresholds
 SizeArenaX = 600
 SizeArenaY = 250
 ThreshXY = 0.5
 ThreshTheta = 30
 ThreshGoalState = 1.5
 
-
+# Initialize Node Array
 node_array = np.array([[[ 0 for k in range(int(360/ThreshTheta))] 
                         for j in range(int(SizeArenaX/ThreshXY))] 
                         for i in range(int(SizeArenaY/ThreshXY))])
 
 Open_List = PriorityQueue() #Initialize list using priority queue.
-traversed_nodes = []
-starting_node_Temp = Node(InitState, None, None, 0, 0) #Generate starting node based on the initial state given above.
+traversed_nodes = [] #Traversed nodes is for visualization later.
+starting_node_Temp = Node(InitState, None, None, 0, 0) #Generate starting node based on the initial state given above. The Temp node is needed for plotting.
 starting_node = Node(InitState, starting_node_Temp, None, 0, Calculate_C2G(InitState, GoalState)) #Generate starting node based on the initial state given above.
 Open_List.put((starting_node.ReturnTotalCost(), starting_node)) #Add to Open List
 
 
 GoalReach = False #Initialze Goal Check Variable
 
-Closed_List= np.array([])#Initialize Closed List of nodes, size of workspace, and setting their cost to infinity to allow for Dijkstra searching.
+Closed_List= np.array([])#Initialize Closed List of nodes. Closed list is based on node states
 
 ##-----------------------CONDUCT A*---------------------##
 
@@ -453,51 +455,38 @@ print("A* Search Starting!!!!")
 
 while not (Open_List.empty()):
     current_node = Open_List.get()[1] #Grab first (lowest cost) item from Priority Queue.
-    traversed_nodes.append(current_node)
-    Plotter(current_node.ReturnState(), current_node.ReturnParentState(), 'g')
-
-
-
-
-
-    print(current_node.ReturnState(), current_node.ReturnTotalCost())
-    np.append(Closed_List, current_node.ReturnState())
-
-
-
+    traversed_nodes.append(current_node) #Append the explored node (for visualization later)
+    Plotter(current_node.ReturnState(), current_node.ReturnParentState(), 'g') #Plot the move
+    print(current_node.ReturnState(), current_node.ReturnTotalCost()) #Print to show search is working.
+    np.append(Closed_List, current_node.ReturnState()) #Append to Closed List
     goalreachcheck = CompareToGoal(current_node.ReturnState(), GoalState, ThreshGoalState) #Check if we have reached goal.
 
     if goalreachcheck: #If we have reached goal node.
         print("Goal Reached!")
         print("Total Cost:", current_node.ReturnTotalCost()) #Print Total Cost
         MovesPath, Path = current_node.ReturnPath() #BackTrack to find path.
-        for nodes in Path:
-            Plotter(nodes.ReturnState(), nodes.ReturnParentState(), 'm')
+        for nodes in Path: #For Each node in ideal path
+            Plotter(nodes.ReturnState(), nodes.ReturnParentState(), 'm') #Plot in Magenta
 
-
-        ##----------Extend Video a Little Longer to see Everything--------##
-        for i in range(200):
-            video.write(cv.cvtColor(arena, cv.COLOR_RGB2BGR))
-
-        break
 
 
     else: #If you have NOT reached the goal node
         NewNodeStates = GeneratePossibleMoves(current_node, StepSize, RobotRadius)#Generate New Nodes from the possible moves current node can take.
-        ParentC2C = current_node.ReturnC2C()
+        ParentC2C = current_node.ReturnC2C() #Get Parent C2C
         if NewNodeStates not in Closed_List: #Check to see if the new node position is currently in the closed list
             for State in NewNodeStates: #For each new node generated by the possible moves.
-                ChildNode_C2C = ParentC2C + StepSize
-                ChildNode_Total_Cost = ChildNode_C2C + Calculate_C2G(State, GoalState)
-                NewChild = Node(State, current_node, "Move" ,ChildNode_C2C, ChildNode_Total_Cost)
-                if CheckIfVisited(NewChild.ReturnState(), node_array, ThreshXY, ThreshTheta) ==  False:
+                ChildNode_C2C = ParentC2C + StepSize #Get C2C for the child node
+                ChildNode_Total_Cost = ChildNode_C2C + Calculate_C2G(State, GoalState) #Get Total Cost for Child Node
+                NewChild = Node(State, current_node, "Move" ,ChildNode_C2C, ChildNode_Total_Cost) #Generate New Child Node Class
+                if CheckIfVisited(NewChild.ReturnState(), node_array, ThreshXY, ThreshTheta) ==  False: #If the node has not been visited before
+                    #Mark in Node Array
                     node_array[int(Round2Half(NewChild.ReturnState()[1])/ThreshXY), int(Round2Half(NewChild.ReturnState()[0])/ThreshXY), int(Round2Half(NewChild.ReturnState()[2])/ThreshTheta)] = 1
-                    Open_List.put((NewChild.ReturnTotalCost() , NewChild))
-                if CheckIfVisited(NewChild.ReturnState(), node_array, ThreshXY, ThreshTheta) ==  True:
-                        if NewChild.ReturnTotalCost() > current_node.ReturnC2C() + StepSize:
-                            NewChild.parent = current_node
-                            NewChild.C2C = current_node.ReturnC2C() + StepSize
-                            NewChild.TotalCost = NewChild.ReturnC2C() + Calculate_C2G(NewChild.ReturnState(), GoalState)
+                    Open_List.put((NewChild.ReturnTotalCost() , NewChild)) #Put it into the Open list
+                if CheckIfVisited(NewChild.ReturnState(), node_array, ThreshXY, ThreshTheta) ==  True: #If you have visited before:
+                        if NewChild.ReturnTotalCost() > current_node.ReturnC2C() + StepSize: #If the current total cost is greater than the move
+                            NewChild.parent = current_node #Update Parent
+                            NewChild.C2C = current_node.ReturnC2C() + StepSize #Update C2C
+                            NewChild.TotalCost = NewChild.ReturnC2C() + Calculate_C2G(NewChild.ReturnState(), GoalState) #Update Total Cost
 
     if goalreachcheck: #If you reach goal
         break #Break the Loop
@@ -505,24 +494,25 @@ while not (Open_List.empty()):
 stoptime = timeit.default_timer() #Stop the Timer, as Searching is complete.
 print("That took", stoptime - starttime, "seconds to complete")
 
-video.release()
 
+#Show the Completed Searched Arena
 plt.imshow(arena, origin='lower')
 plt.show()
 
 
 ##--------------------Visualization--------------------##
-plt.plot(InitState[0], InitState[1], 'go', markersize = 0.5)
+print("Visualization Starting!")
+plt.plot(InitState[0], InitState[1], 'go', markersize = 0.5) #plot init state
 plt.imshow(arena, origin = 'lower')
 
-for node in traversed_nodes:
+for node in traversed_nodes: #Plots the search area
     curr_node_state = node.ReturnState()
     parent_node_state = node.ReturnParentState()
     Plotter(curr_node_state, parent_node_state, 'g')
     plt.pause(0.00001)
     
 
-for node in Path:
+for node in Path: #Plots the ideal path
     curr_node_state = node.ReturnState()
     parent_node_state = node.ReturnParentState()
     Plotter(curr_node_state, parent_node_state, 'm')
